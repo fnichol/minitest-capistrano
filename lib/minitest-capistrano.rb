63c5f67986841a9122ebad672dda95f793a16859
Fletcher Nichol <fnichol@nichol.ca>
2012-01-05 11:23:18 -0700

    Initial import from `bundle gem minitest-capistrano`.

---
+++ lib/minitest-capistrano.rb
@@ -0,0 +1,7 @@
+require "minitest-capistrano/version"
+
+module Minitest
+  module Capistrano
+    # Your code goes here...
+  end
+end
---
+++ lib/minitest-capistrano/version.rb
@@ -0,0 +1,5 @@
+module Minitest
+  module Capistrano
+    VERSION = "0.0.1"
+  end
+end
