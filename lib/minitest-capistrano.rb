ac5ba7c4ff0e038b95c6fa8299794fac2d7bcf21
Shaun Dern <shaun@substantial.com>
2012-04-25 10:02:19 -0700

    adding loading_recipe helper

--- lib/minitest-capistrano.rb
+++ lib/minitest-capistrano.rb
@@ -1,2 +1,19 @@
 require "minitest/capistrano"
 require "minitest/capistrano/version"
+
+def load_capistrano_recipe(recipe)
+  before do
+    @config = Capistrano::Configuration.new
+    @config.extend(MiniTest::Capistrano::ConfigurationExtension)
+    @orig_config = Capistrano::Configuration.instance
+    Capistrano::Configuration.instance = @config
+    recipe.send(:load_into, @config)
+  end
+
+  after do
+    Capistrano::Configuration.instance = @orig_config
+  end
+
+  subject {@config}
+  let(:recipe) { @config }
+end
